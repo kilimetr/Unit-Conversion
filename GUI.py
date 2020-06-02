77d0e08e10fd4cdacda2272be8d17dc333a6315f
Dominik Capkovic <domcapkovic@gmail.com>
2020-06-02 21:41:12 +0200

    02.06.2020 version

--- GUI.py
+++ GUI.py
@@ -3427,7 +3427,211 @@ class Massflow_Win(QtWidgets.QWidget):
 class Massfrac_Win(QtWidgets.QWidget):
 	def __init__(self, parent = None):
 		super().__init__()
-		self.setWindowTitle("Massfrac_Win")
+
+		self.setWindowTitle("Mass Fraction Converter")
+		self.main_window()
+
+	def createGroup_input(self):
+		input_group = QtWidgets.QGroupBox("Input", self)
+		input_group.setFont(QtGui.QFont("Arial", 13, QtGui.QFont.Black))
+
+		input_grid = QtWidgets.QGridLayout()
+		input_grid.setColumnMinimumWidth(0, 130)
+
+		blanklabel          = QtWidgets.QLabel()
+		self.decimali_LinEd = QtWidgets.QLineEdit()
+		self.percenti_LinEd = QtWidgets.QLineEdit()
+		self.promilei_LinEd = QtWidgets.QLineEdit()
+		self.ppmi_LinEd     = QtWidgets.QLineEdit()
+		self.ppbi_LinEd     = QtWidgets.QLineEdit()
+
+		decimal_Label = QtWidgets.QLabel("decimal (1)")
+		percent_Label = QtWidgets.QLabel("%")
+		promile_Label = QtWidgets.QLabel("‰")
+		ppm_Label     = QtWidgets.QLabel("ppm")
+		ppb_Label     = QtWidgets.QLabel("ppb")
+
+		input_grid.addWidget(blanklabel,          0, 0)
+		input_grid.addWidget(self.decimali_LinEd, 1, 0)
+		input_grid.addWidget(self.percenti_LinEd, 2, 0)
+		input_grid.addWidget(self.promilei_LinEd, 3, 0)
+		input_grid.addWidget(self.ppmi_LinEd,     4, 0)
+		input_grid.addWidget(self.ppbi_LinEd,     5, 0)
+
+		input_grid.addWidget(decimal_Label, 1, 1)
+		input_grid.addWidget(percent_Label, 2, 1)
+		input_grid.addWidget(promile_Label, 3, 1)
+		input_grid.addWidget(ppm_Label,     4, 1)
+		input_grid.addWidget(ppb_Label,     5, 1)
+
+		input_group.setLayout(input_grid)
+
+		# self.m2i_LinEd.returnPressed.connect(self.m2TO_fun)
+
+		return input_group
+
+	def createGroup_output(self):
+		output_group = QtWidgets.QGroupBox("Output", self)
+		output_group.setFont(QtGui.QFont("Arial", 13, QtGui.QFont.Black))
+
+		output_grid = QtWidgets.QGridLayout()
+		output_grid.setColumnMinimumWidth(0, 130)
+
+		decimal_Label = QtWidgets.QLabel("decimal (1)")
+		percent_Label = QtWidgets.QLabel("%")
+		promile_Label = QtWidgets.QLabel("‰")
+		ppm_Label     = QtWidgets.QLabel("ppm")
+		ppb_Label     = QtWidgets.QLabel("ppb")
+
+		self.decimalTOdecimal_res = QtWidgets.QLabel("0", self)
+		self.decimalTOpercent_res = QtWidgets.QLabel("0", self)
+		self.decimalTOpromile_res = QtWidgets.QLabel("0", self)
+		self.decimalTOppm_res     = QtWidgets.QLabel("0", self)
+		self.decimalTOppb_res     = QtWidgets.QLabel("0", self)
+
+		self.percentTOdecimal_res = QtWidgets.QLabel("0", self)
+		self.percentTOpercent_res = QtWidgets.QLabel("0", self)
+		self.percentTOpromile_res = QtWidgets.QLabel("0", self)
+		self.percentTOppm_res     = QtWidgets.QLabel("0", self)
+		self.percentTOppb_res     = QtWidgets.QLabel("0", self)
+
+		self.promileTOdecimal_res = QtWidgets.QLabel("0", self)
+		self.promileTOpercent_res = QtWidgets.QLabel("0", self)
+		self.promileTOpromile_res = QtWidgets.QLabel("0", self)
+		self.promileTOppm_res     = QtWidgets.QLabel("0", self)
+		self.promileTOppb_res     = QtWidgets.QLabel("0", self)
+
+		self.ppmTOdecimal_res = QtWidgets.QLabel("0", self)
+		self.ppmTOpercent_res = QtWidgets.QLabel("0", self)
+		self.ppmTOpromile_res = QtWidgets.QLabel("0", self)
+		self.ppmTOppm_res     = QtWidgets.QLabel("0", self)
+		self.ppmTOppb_res     = QtWidgets.QLabel("0", self)
+
+		self.ppbTOdecimal_res = QtWidgets.QLabel("0", self)
+		self.ppbTOpercent_res = QtWidgets.QLabel("0", self)
+		self.ppbTOpromile_res = QtWidgets.QLabel("0", self)
+		self.ppbTOppm_res     = QtWidgets.QLabel("0", self)
+		self.ppbTOppb_res     = QtWidgets.QLabel("0", self)
+
+		output_grid.addWidget(decimal_Label, 0, 0)
+		output_grid.addWidget(percent_Label, 0, 1)
+		output_grid.addWidget(promile_Label, 0, 2)
+		output_grid.addWidget(ppm_Label,     0, 3)
+		output_grid.addWidget(ppb_Label,     0, 4)
+
+		output_grid.addWidget(self.decimalTOdecimal_res, 1, 0)
+		output_grid.addWidget(self.decimalTOpercent_res, 1, 1)
+		output_grid.addWidget(self.decimalTOpromile_res, 1, 2)
+		output_grid.addWidget(self.decimalTOppm_res,     1, 3)
+		output_grid.addWidget(self.decimalTOppb_res,     1, 4)
+
+		output_grid.addWidget(self.percentTOdecimal_res, 2, 0)
+		output_grid.addWidget(self.percentTOpercent_res, 2, 1)
+		output_grid.addWidget(self.percentTOpromile_res, 2, 2)
+		output_grid.addWidget(self.percentTOppm_res,     2, 3)
+		output_grid.addWidget(self.percentTOppb_res,     2, 4)
+
+		output_grid.addWidget(self.promileTOdecimal_res, 3, 0)
+		output_grid.addWidget(self.promileTOpercent_res, 3, 1)
+		output_grid.addWidget(self.promileTOpromile_res, 3, 2)
+		output_grid.addWidget(self.promileTOppm_res,     3, 3)
+		output_grid.addWidget(self.promileTOppb_res,     3, 4)
+
+		output_grid.addWidget(self.ppmTOdecimal_res, 4, 0)
+		output_grid.addWidget(self.ppmTOpercent_res, 4, 1)
+		output_grid.addWidget(self.ppmTOpromile_res, 4, 2)
+		output_grid.addWidget(self.ppmTOppm_res,     4, 3)
+		output_grid.addWidget(self.ppmTOppb_res,     4, 4)
+
+		output_grid.addWidget(self.ppbTOdecimal_res, 5, 0)
+		output_grid.addWidget(self.ppbTOpercent_res, 5, 1)
+		output_grid.addWidget(self.ppbTOpromile_res, 5, 2)
+		output_grid.addWidget(self.ppbTOppm_res,     5, 3)
+		output_grid.addWidget(self.ppbTOppb_res,     5, 4)
+
+		output_group.setLayout(output_grid)
+
+		self.decimali_LinEd.returnPressed.connect(self.decimalTO_fun)
+		self.percenti_LinEd.returnPressed.connect(self.percentTO_fun)
+		self.promilei_LinEd.returnPressed.connect(self.promileTO_fun)
+		self.ppmi_LinEd.returnPressed.connect(self.ppmTO_fun)
+		self.ppbi_LinEd.returnPressed.connect(self.ppbTO_fun)
+
+		return output_group
+
+	def decimalTO_fun(self):
+		decimalTOdecimal_proc = float(self.decimali_LinEd.text()) * 1
+		decimalTOpercent_proc = float(self.decimali_LinEd.text()) * 1e-02
+		decimalTOpromile_proc = float(self.decimali_LinEd.text()) * 1e-03
+		decimalTOppm_proc     = float(self.decimali_LinEd.text()) * 1e-06
+		decimalTOppb_proc     = float(self.decimali_LinEd.text()) * 1e-09
+
+		self.decimalTOdecimal_res.setText(str(round(decimalTOdecimal_proc, 8)))
+		self.decimalTOpercent_res.setText(str(round(decimalTOpercent_proc, 8)))
+		self.decimalTOpromile_res.setText(str(round(decimalTOpromile_proc, 8)))
+		self.decimalTOppm_res.setText(str(round(decimalTOppm_proc,         8)))
+		self.decimalTOppb_res.setText(str(decimalTOppb_proc                  ))
+
+	def percentTO_fun(self):
+		percentTOdecimal_proc = float(self.percenti_LinEd.text()) * 1e+02
+		percentTOpercent_proc = float(self.percenti_LinEd.text()) * 1
+		percentTOpromile_proc = float(self.percenti_LinEd.text()) * 1e-01
+		percentTOppm_proc     = float(self.percenti_LinEd.text()) * 1e-04
+		percentTOppb_proc     = float(self.percenti_LinEd.text()) * 1e-07
+
+		self.percentTOdecimal_res.setText(str(round(percentTOdecimal_proc, 8)))
+		self.percentTOpercent_res.setText(str(round(percentTOpercent_proc, 8)))
+		self.percentTOpromile_res.setText(str(round(percentTOpromile_proc, 8)))
+		self.percentTOppm_res.setText(str(round(percentTOppm_proc,         8)))
+		self.percentTOppb_res.setText(str(round(percentTOppb_proc,         8)))
+
+	def promileTO_fun(self):
+		promileTOdecimal_proc = float(self.promilei_LinEd.text()) * 1e+03
+		promileTOpercent_proc = float(self.promilei_LinEd.text()) * 1e+01
+		promileTOpromile_proc = float(self.promilei_LinEd.text()) * 1
+		promileTOppm_proc     = float(self.promilei_LinEd.text()) * 1e-03
+		promileTOppb_proc     = float(self.promilei_LinEd.text()) * 1e-06
+
+		self.promileTOdecimal_res.setText(str(round(promileTOdecimal_proc, 8)))
+		self.promileTOpercent_res.setText(str(round(promileTOpercent_proc, 8)))
+		self.promileTOpromile_res.setText(str(round(promileTOpromile_proc, 8)))
+		self.promileTOppm_res.setText(str(round(promileTOppm_proc,         8)))
+		self.promileTOppb_res.setText(str(round(promileTOppb_proc,         8)))
+
+	def ppmTO_fun(self):
+		ppmTOdecimal_proc = float(self.ppmi_LinEd.text()) * 1e+06
+		ppmTOpercent_proc = float(self.ppmi_LinEd.text()) * 1e+04
+		ppmTOpromile_proc = float(self.ppmi_LinEd.text()) * 1e+03
+		ppmTOppm_proc     = float(self.ppmi_LinEd.text()) * 1
+		ppmTOppb_proc     = float(self.ppmi_LinEd.text()) * 1e-03
+
+		self.ppmTOdecimal_res.setText(str(round(ppmTOdecimal_proc, 8)))
+		self.ppmTOpercent_res.setText(str(round(ppmTOpercent_proc, 8)))
+		self.ppmTOpromile_res.setText(str(round(ppmTOpromile_proc, 8)))
+		self.ppmTOppm_res.setText(str(round(ppmTOppm_proc,         8)))
+		self.ppmTOppb_res.setText(str(round(ppmTOppb_proc,         8)))
+
+	def ppbTO_fun(self):
+		ppbTOdecimal_proc = float(self.ppbi_LinEd.text()) * 1e+09
+		ppbTOpercent_proc = float(self.ppbi_LinEd.text()) * 1e+07
+		ppbTOpromile_proc = float(self.ppbi_LinEd.text()) * 1e+06
+		ppbTOppm_proc     = float(self.ppbi_LinEd.text()) * 1e+03
+		ppbTOppb_proc     = float(self.ppbi_LinEd.text()) * 1
+
+		self.ppbTOdecimal_res.setText(str(round(ppbTOdecimal_proc, 8)))
+		self.ppbTOpercent_res.setText(str(round(ppbTOpercent_proc, 8)))
+		self.ppbTOpromile_res.setText(str(round(ppbTOpromile_proc, 8)))
+		self.ppbTOppm_res.setText(str(round(ppbTOppm_proc,         8)))
+		self.ppbTOppb_res.setText(str(round(ppbTOppb_proc,         8)))
+
+	def main_window(self):
+		main_layout = QtWidgets.QHBoxLayout()
+		main_layout.addWidget(self.createGroup_input())
+		main_layout.addWidget(self.createGroup_output())
+
+		self.setLayout(main_layout)
+		self.show()
+
 
 class Powheatfl_Win(QtWidgets.QWidget):
 	def __init__(self, parent = None):
